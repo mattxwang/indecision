99d4067e329bcd81634058d12233013226d1dde5
Matthew Wang <matt@matthewwang.me>
2023-04-10 20:19:41 -0700

    Standardize `Select`

--- src/vtree/VTreeSelect.tsx
+++ src/vtree/VTreeSelect.tsx
@@ -21,7 +21,7 @@ export default function VTreeSelect({ setVTreeType }: Props): JSX.Element {
 
   return (
     <>
-      <div className="inline-block relative mr-2">
+      <div className="inline-block mr-2">
         <select
           value={selectedType}
           onChange={(e) => {
@@ -29,7 +29,7 @@ export default function VTreeSelect({ setVTreeType }: Props): JSX.Element {
             setSelectedType(n)
             setVTreeType(n === 'EvenSplit' ? { EvenSplit: numSplits } : n)
           }}
-          className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          className="btn btn-blue w-full h-full"
         >
           {vtreeTypeOptions.map((option) => (
             <option value={option.value} key={option.label}>
@@ -37,15 +37,6 @@ export default function VTreeSelect({ setVTreeType }: Props): JSX.Element {
             </option>
           ))}
         </select>
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg
-            className="fill-current h-4 w-4"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-          </svg>
-        </div>
       </div>
       {selectedType === 'EvenSplit' && (
         <>
